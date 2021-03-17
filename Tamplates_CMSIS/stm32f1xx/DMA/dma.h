e07c53a4b6d56b0864fa7792d3c569894cc54eef
Kolantaevev <Kolantaevev@gmail.com>
2021-03-17 23:10:07 +0300

    DMA stm32f4 changed

--- Tamplates_CMSIS/stm32f1xx/DMA/dma.h
+++ Tamplates_CMSIS/stm32f1xx/DMA/dma.h
@@ -1,28 +1,9 @@
-#ifndef DMA
-#define DMA
-#define DMA8_size 128
-#define DMA16_size 128
-#define DMA32_size 128
+#ifndef LIB_DMA
+#define LIB_DMA
 
 #include "stm32f1xx.h"
 #include "string.h"
 
-typedef struct {
-    uint8_t data[DMA8_size];
-    uint16_t size;
-} DMA8_buff;
+void DMA1_Ch2_init(volatile void *addr_perif, volatile void *addr_buff);
 
-typedef struct {
-    uint16_t data[DMA16_size];
-    uint16_t size;
-} DMA16_buff;
-
-typedef struct {
-    uint32_t data[DMA32_size];
-    uint16_t size;
-} DMA32_buff;
-
-void DMA_init(void);
-void DMA_buff_clear(void *buff);
-
-#endif
+#endif  /*LIB_DMA*/
