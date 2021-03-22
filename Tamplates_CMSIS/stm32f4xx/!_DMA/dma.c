f8ed8ccb55c537f1b4bc22fd3a73667ada934461
KolantaevEV <kolantaevev@gmail.com>
2021-03-22 13:43:32 +0300

    add stm32f407vg dma tamplate

--- Tamplates_CMSIS/stm32f4xx/!_DMA/dma.c
+++
@@ -1,9 +0,0 @@
-#include "dma.h"
-
-void DMA2_S5_init(volatile void *addr_perif, volatile void *addr_buff)
-{
-    RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-
-    DMA2->
-    DMA2_Stream1-> = (uint32_t)addr_perif;
-}
--- Tamplates_CMSIS/stm32f4xx/DMA/dma.h
+++ Tamplates_CMSIS/stm32f4xx/DMA/dma.h
@@ -4,7 +4,7 @@
 #include "stm32f4xx.h"
 #include "string.h"
 
-void DMA2_S5_init(volatile void *addr_perif, volatile void *addr_buff);
+void DMA2_S2_init(volatile void *addr_perif, volatile void *addr_buff);
 void DMA2_S7_init(volatile void *addr_perif, volatile void *addr_buff);
 
 #endif  /*LIB_DMA*/
